181e82dd6610104d504c656b71a94b28ffe4dca9
Callum Whyte <hey@callumwhyte.com>
2021-06-29 16:59:51 +0100

    Creating lazy initialised KeyValue collection builder

---
+++ src/Our.Umbraco.Extensions.Composing/LazyKeyValueCollection.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Our.Umbraco.Extensions.Composing
+{
+    public class LazyKeyValueCollection<TItem> : LazyKeyValueCollection<string, TItem>
+    {
+        public LazyKeyValueCollection(IDictionary<string, Lazy<TItem>> collection)
+            : base(collection)
+        {
+
+        }
+    }
+
+    public class LazyKeyValueCollection<TKey, TItem> : IKeyValueCollection<TKey, TItem>
+    {
+        private readonly IDictionary<TKey, Lazy<TItem>> _collection;
+
+        public LazyKeyValueCollection(IDictionary<TKey, Lazy<TItem>> collection)
+        {
+            _collection = collection;
+        }
+
+        public TItem this[TKey key] => _collection[key].Value;
+
+        public bool TryGetValue(TKey key, out TItem value)
+        {
+            if (_collection.TryGetValue(key, out Lazy<TItem> lazyValue) == true)
+            {
+                value = lazyValue.Value;
+            }
+            else
+            {
+                value = default(TItem);
+            }
+
+            return value != null;
+        }
+
+        #region IEnumerable
+
+        public int Count => _collection.Count;
+
+        public IEnumerator<TItem> GetEnumerator() => _collection.Values
+            .Select(x => x.Value)
+            .GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        #endregion
+    }
+}
---
+++ src/Our.Umbraco.Extensions.Composing/LazyKeyValueCollectionBuilder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Our.Umbraco.Extensions.Composing
+{
+    public class LazyKeyValueCollectionBuilder<TBuilder, TCollection, TItem> : LazyKeyValueCollectionBuilder<TBuilder, TCollection, string, TItem>
+        where TBuilder : LazyKeyValueCollectionBuilder<TBuilder, TCollection, TItem>
+        where TCollection : class, IKeyValueCollection<string, TItem>
+    {
+
+    }
+
+    public class LazyKeyValueCollectionBuilder<TBuilder, TCollection, TKey, TItem> : KeyValueCollectionBuilder<TBuilder, TCollection, TKey, TItem>
+        where TBuilder : LazyKeyValueCollectionBuilder<TBuilder, TCollection, TKey, TItem>
+        where TCollection : class, IKeyValueCollection<TKey, TItem>
+    {
+        public override TCollection CreateCollection(IFactory factory)
+        {
+            Func<Type, Lazy<TItem>> createLazy = type
+                => new Lazy<TItem>(() => CreateItem(factory, type));
+
+            var items = GetItems().ToDictionary(x => x.Key, x => createLazy(x.Value));
+
+            return factory.CreateInstance<TCollection>(items);
+        }
+    }
+}
